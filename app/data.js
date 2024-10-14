fa3042a44c983d852790db35f959447a200794f9
Mhd-alali <hsinsa758@gmail.com>
2024-10-14 12:36:49 +0300

    lio's modifications

--- app/data.js
+++ app/data.js
@@ -1,56 +1,7 @@
 
 export const data = [
     {
-        images: [
-            '/1/1.webp',
-            '/1/2.webp',
-            '/1/3.webp',
-            '/1/4.webp',
-            '/1/5.webp',
-            '/1/6.webp',
-            '/1/7.webp',
-            '/1/8.webp',
-        ]
-    },
-    {
-        images: [
-            '/2/1.webp',
-            '/2/2.webp',
-            '/2/3.webp',
-            '/2/4.webp',
-            '/2/5.webp',
-            '/2/6.webp',
-            '/2/7.webp',
-            '/2/8.webp',
-            '/2/9.webp',
-            '/2/10.webp',
-            '/2/11.webp',
-            '/2/12.webp',
-            '/2/13.webp',
-            '/2/14.webp',
-            '/2/15.webp',
-            '/2/16.webp',
-            '/2/17.webp',
-            '/2/18.webp',
-            '/2/19.webp',
-        ]
-    },
-    {
-        images: [
-            '/3/1.webp',
-            '/3/2.webp',
-            '/3/3.webp',
-            '/3/4.webp',
-            '/3/5.webp',
-            '/3/6.webp',
-            '/3/7.webp',
-            '/3/8.webp',
-            '/3/9.webp',
-            '/3/10.webp',
-            '/3/11.webp',
-        ]
-    },
-    {
+        title:"books covers",
         images: [
             '/4/1.webp',
             '/4/2.webp',
@@ -64,6 +15,55 @@ export const data = [
             '/4/10.webp',
             '/4/11.webp',
             '/4/12.webp',
+            '/4/13.webp',
+            '/4/14.webp',
+            '/4/1.webp',
         ]
     },
+    {
+        title:"sleeping beauty",
+        images: [
+            '/1/1.webp',
+            '/1/4.webp',
+            '/1/5.webp',
+            '/1/6.webp',
+            '/1/7.webp',
+            '/1/8.webp',
+        ]
+    },
+    {
+        title:"Sia stories",
+        images: [
+            '/2/1.webp',
+            '/2/18.webp',
+            '/2/17.webp',
+            '/2/16.webp',
+            '/2/15.webp',
+            '/2/14.webp',
+            '/2/13.webp',
+            '/2/12.webp',
+            '/2/11.webp',
+            '/2/10.webp',
+            '/2/9.webp',
+            '/2/8.webp',
+            '/2/7.webp',
+            '/2/6.webp',
+            '/2/5.webp',
+            '/2/4.webp',
+        ]
+    },
+    {
+        title:"golden duck",
+        images: [
+            '/3/1.webp',
+            '/3/10.webp',
+            '/3/9.webp',
+            '/3/8.webp',
+            '/3/7.webp',
+            '/3/6.webp',
+            '/3/5.webp',
+            '/3/4.webp',
+        ]
+    },
+    
 ];
--- app/page.jsx
+++ app/page.jsx
@@ -2,17 +2,25 @@ import Image from "next/image";
 import { data } from "./data";
 
 export default function Home() {
-
+  
   return (
     <div className="">
-      <h1 className='text-5xl text-center my-10'>Lio Gallery</h1>
+      <h1 className='text-5xl text-center my-10'>Lio Gallery (children books)</h1>
       <div className="space-y-52">
         {data.map((datum, idx) => (
-          <section key={idx} className="flex flex-wrap items-center justify-center gap-8">
-            {datum.images.map((image, idx) => (
-              <Image className="object-cover w-full md:max-w-[calc(50%-1rem)]" key={idx} src={image} alt={`lio gallery`} placeholder="blur" blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN0cfepBwADBgFYlcy+XgAAAABJRU5ErkJggg==" width={500} height={250} quality={100} loading="lazy" />
+          <div key={idx} >
+          <h1 className='text-5xl text-center my-10'>{datum.title}</h1>
+          <section className="flex flex-col items-center justify-center gap-8">
+            {datum.images.map((image, imageIdx) => (
+              <>
+              {(idx > 0 && imageIdx === 0) ?
+              <Image className="object-cover w-full" key={idx} src={image} alt={`lio gallery`} placeholder="blur" blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN0cfepBwADBgFYlcy+XgAAAABJRU5ErkJggg==" width={800} height={250} quality={100} loading="lazy" />
+              :
+              <Image className="object-cover w-full md:max-w-[calc(50%-1rem)]" key={idx} src={image} alt={`lio gallery`} placeholder="blur" blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN0cfepBwADBgFYlcy+XgAAAABJRU5ErkJggg==" width={500} height={250} quality={100} loading="lazy" />}
+              </>
             ))}
           </section>
+            </div>
         ))}
       </div>
     </div>
